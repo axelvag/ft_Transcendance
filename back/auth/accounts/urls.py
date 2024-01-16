6aeb253ed983e3f624eba2a2948365ddf3364e6f
Lucas-Ve <lveloso@student.42lyon.fr>
2024-01-16 11:45:30 +0100

    [#8][authentification] forget password done and start of the design css

--- back/auth/accounts/urls.py
+++ back/auth/accounts/urls.py
@@ -8,8 +8,4 @@ urlpatterns = [
     path('logout/', views.logout_user, name='logout_user'),
     path('register/', views.register_user, name='register_user'),
     path('activate/<uidb64>/<token>', views.activate, name='activate'),
-    # path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    # path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    # path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    # path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
--- back/auth/auth/urls.py
+++ back/auth/auth/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('Home/', include('Home.urls')),
-    path('accounts/', include('accounts.urls'))
+    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
