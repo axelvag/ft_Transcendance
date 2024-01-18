c32039e837ab8021b9a008763d07fd3c19944a82
ascatama@student.42lyon.fr <ascatama@student.42lyon.fr>
2024-01-18 15:52:52 +0100

    [#34][Front] Catch when user click save and reset fiel when user click nnuler

--- front/public/views/view-profil.js
+++ front/public/views/view-profil.js
@@ -1,43 +1,3 @@
-// class ViewProfil extends HTMLElement {
-//     connectedCallback() {
-//         this.innerHTML = `
-//       <main class="profile-container">
-
-//         <section class="profile-header">
-//             <h1>Edit profile</h1>
-//             <button class="avatar-button">Change avatar</button>
-//         </section>
-
-//         <form class="profile-form">
-        
-//             <div class="form-group">
-//                 <label for="first-name">First Name</label>
-//                 <input type="text" id="first-name" value="toto">
-//             </div>
-//             <div class="form-group">
-//                 <label for="last-name">Last Name</label>
-//                 <input type="text" id="last-name" value="toto">
-//             </div>
-//             <div class="form-group">
-//                 <label for="email">Email</label>
-//                 <input type="email" id="email" value="toto@gmail.com">
-//             </div>
-//             <div class="form-group">
-//                 <label for="password">Password</label>
-//                 <input type="password" id="password" value="sdbfbnd65sfdvb s">
-//             </div>
-//             <div class="form-actions">
-//                 <button type="button" class="cancel-button">Cancel</button>
-//                 <button type="submit" class="save-button">Save</button>
-//             </div>
-//         </form>
-//       </main>
-//     `;
-//     }
-// }
-  
-//   customElements.define('view-profil', ViewProfil);
-
 class ViewProfil extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
@@ -52,27 +12,75 @@ class ViewProfil extends HTMLElement {
             
                 <div class="form-group">
                     <label for="first-name">First Name</label>
-                    <input type="text" id="first-name" value="toto">
+                    <input type="text" id="first-name" value="">
                 </div>
                 <div class="form-group">
                     <label for="last-name">Last Name</label>
-                    <input type="text" id="last-name" value="toto">
+                    <input type="text" id="last-name" value="">
                 </div>
                 <div class="form-group">
                     <label for="email">Email</label>
-                    <input type="email" id="email" value="toto@gmail.com">
+                    <input type="email" id="email" value="">
                 </div>
                 <div class="form-group">
                     <label for="password">Password</label>
-                    <input type="password" id="password" value="sdbfbnd65sfdvb s">
+                    <input type="password" id="password" value="">
                 </div>
                 <div class="form-actions">
-                    <button type="button" class="cancel-button">Cancel</button>
-                    <button type="submit" class="save-button">Save</button>
+                    <button type="cancel-button" class="cancel-button">Annuler</button>
+                    <button type="submit" class="save-button">Sauvegarder</button>
                 </div>
             </form>
         </main>
         `;
+
+        const cancelButton = this.querySelector('.cancel-button');
+        const profileForm = this.querySelector('.profile-form');
+    
+        cancelButton.addEventListener('click', () => {
+            profileForm.reset(); // Réinitialise le formulaire
+        });
+    
+        profileForm.addEventListener('submit', this.saveProfile);
+    }
+    
+    saveProfile(event) {
+        event.preventDefault();
+
+        const firstName = this.querySelector('#first-name').value;
+        const lastName = this.querySelector('#last-name').value;
+        const email = this.querySelector('#email').value;
+        const password = this.querySelector('#password').value;
+
+        //Tests
+        console.log('First Name:', firstName);
+        console.log('Last Name:', lastName);
+        console.log('Email:', email);
+        console.log('Password:', password);
+
+        // Ici, vous pouvez appeler la méthode fetch pour envoyer les données
+        // à votre serveur
+        // fetch('/path_to_your_server_endpoint', {
+        //     method: 'POST',
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //     },
+        //     body: JSON.stringify({
+        //         firstName: firstName,
+        //         lastName: lastName,
+        //         email: email,
+        //         password: password
+        //     })
+        // })
+        // .then(response => response.json())
+        // .then(data => {
+        //     // Traiter la réponse ici
+        //     console.log('Success:', data);
+        // })
+        // .catch((error) => {
+        //     console.error('Error:', error);
+        // });
+
     }
 }
 
