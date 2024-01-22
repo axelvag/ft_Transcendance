fa98220ce778c553e2fe65c96e2065f693044883
Lucas-Ve <lveloso@student.42lyon.fr>
2024-01-22 15:44:54 +0100

    [#8][authentification] json response done and test unitaire in progress

--- back/auth/accounts/views.py
+++ back/auth/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.http import JsonResponse
 from django.template import loader
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import authenticate, login, logout
@@ -13,27 +14,104 @@ from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes, force_str
 from django.core.mail import EmailMessage
 from .tokens import account_activation_token
+from django.http import JsonResponse, HttpResponseBadRequest, HttpResponseServerError
 # Create your views here.
 User = get_user_model()
+
+# def activate(request, uidb64, token):
+#     User = get_user_model()
+#     try:
+#         uid = force_str(urlsafe_base64_decode(uidb64))
+#         user = User.objects.get(pk=uid)
+#     except:
+#         user = None
+
+#     if user is not None and account_activation_token.check_token(user,token):
+#         user.is_active = True
+#         user.save()
+
+#         messages.success(request, "Thank you for your email confirmation. Now you can login your account.")
+#         return redirect('accounts:login_user')
+#     else:
+#         messages.error(request, "Activation link is invalid!")
+
+#     return redirect("Home:index")
+
+# def activateEmail(request, user, to_email):
+#     mail_subject = "Activate your user account"
+#     message = render_to_string("template_activate_account.html", {
+#         'user': user.username,
+#         'domain': get_current_site(request).domain,
+#         'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+#         'token': account_activation_token.make_token(user),
+#         "protocol": 'https' if request.is_secure() else 'http'
+#     })
+#     email = EmailMessage(mail_subject, message, to=[to_email])
+#     if email.send():
+#         messages.success(request, f'Dear {user}, please go to you email {to_email} inbox and click on \
+#                 received activation link to confirm and complete the registration. Note: Check your spam folder.')
+#     else:
+#         messages.error(request, f'Problem sending email to {to_email}, check if you typed it correctly.')
+
+# def login_user(request):
+#     if request.method == 'POST':
+#         username_or_email = request.POST["username"]
+#         password = request.POST["password"]
+
+#         # Vérifier si l'entrée est un email ou un nom d'utilisateur
+#         if '@' in username_or_email:
+#             try:
+#                 user = User.objects.get(email=username_or_email)
+#                 username = user.username
+#             except User.DoesNotExist:
+#                 messages.info(request, "Identifiant ou mot de passe incorrect")
+#                 return render(request, "accounts/login.html", {"form": UsernameOrEmailAuthenticationForm()})
+#         else:
+#             username = username_or_email
+
+#         user = authenticate(request, username=username, password=password)
+
+#         if user is not None:
+#             login(request, user)
+#             return redirect("Home:index")
+#         else:
+#             messages.info(request, "Identifiant ou mot de passe incorrect")
+
+#     form = UsernameOrEmailAuthenticationForm()
+#     return render(request, "accounts/login.html", {"form": form})
+
+# def logout_user(request):
+#     logout(request)
+#     return redirect("Home:index")
+
+# def register_user(request):
+#     if request.method == 'POST':
+#         form = UserCreationFormWithEmail(request.POST)
+#         if form.is_valid():
+#             user = form.save(commit=False)
+#             user.is_active=False
+#             user.save()
+#             activateEmail(request, user, form.cleaned_data.get('email'))
+#             return redirect("Home:index")
+#     else:
+#         form = UserCreationFormWithEmail()
+
+#     return render(request, "accounts/register.html", {'form': form})
 
 def activate(request, uidb64, token):
     User = get_user_model()
     try:
         uid = force_str(urlsafe_base64_decode(uidb64))
         user = User.objects.get(pk=uid)
-    except:
-        user = None
+    except Exception as e:
+        return JsonResponse({"success": False, "message": "Invalid activation link."}, status=HttpResponseBadRequest.status_code)
 
-    if user is not None and account_activation_token.check_token(user,token):
+    if user is not None and account_activation_token.check_token(user, token):
         user.is_active = True
         user.save()
-
-        messages.success(request, "Thank you for your email confirmation. Now you can login your account.")
-        return redirect('accounts:login_user')
+        return JsonResponse({"success": True, "message": "Thank you for your email confirmation. Now you can login your account."}, status=200)
     else:
-        messages.error(request, "Activation link is invalid!")
-
-    return redirect("Home:index")
+        return JsonResponse({"success": False, "message": "Activation link is invalid!"}, status=HttpResponseBadRequest.status_code)
 
 def activateEmail(request, user, to_email):
     mail_subject = "Activate your user account"
@@ -46,52 +124,48 @@ def activateEmail(request, user, to_email):
     })
     email = EmailMessage(mail_subject, message, to=[to_email])
     if email.send():
-        messages.success(request, f'Dear {user}, please go to you email {to_email} inbox and click on \
-                received activation link to confirm and complete the registration. Note: Check your spam folder.')
+        return JsonResponse({"success": True, "message": f'Dear {user}, please go to your email {to_email} inbox and click on the received activation link to confirm and complete the registration. Note: Check your spam folder.'}, status=200)
     else:
-        messages.error(request, f'Problem sending email to {to_email}, check if you typed it correctly.')
+        return JsonResponse({"success": False, "message": f'Problem sending email to {to_email}, check if you typed it correctly.'}, status=HttpResponseServerError.status_code)
 
 def login_user(request):
     if request.method == 'POST':
         username_or_email = request.POST["username"]
         password = request.POST["password"]
+        user = None
 
-        # Vérifier si l'entrée est un email ou un nom d'utilisateur
         if '@' in username_or_email:
             try:
                 user = User.objects.get(email=username_or_email)
-                username = user.username
             except User.DoesNotExist:
-                messages.info(request, "Identifiant ou mot de passe incorrect")
-                return render(request, "accounts/login.html", {"form": UsernameOrEmailAuthenticationForm()})
+                pass
         else:
-            username = username_or_email
-
-        user = authenticate(request, username=username, password=password)
+            user = authenticate(request, username=username_or_email, password=password)
 
         if user is not None:
             login(request, user)
-            return redirect("Home:index")
+            return JsonResponse({"success": True, "message": "Login successful."}, status=200)
         else:
-            messages.info(request, "Identifiant ou mot de passe incorrect")
+            return JsonResponse({"success": False, "message": "Invalid username or password."}, status=HttpResponseBadRequest.status_code)
 
-    form = UsernameOrEmailAuthenticationForm()
-    return render(request, "accounts/login.html", {"form": form})
+    return JsonResponse({"success": False, "message": "Invalid request method."}, status=HttpResponseBadRequest.status_code)
 
 def logout_user(request):
     logout(request)
-    return redirect("Home:index")
+    return JsonResponse({"success": True, "message": "You have been logged out successfully."}, status=200)
 
 def register_user(request):
     if request.method == 'POST':
         form = UserCreationFormWithEmail(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
-            user.is_active=False
+            user.is_active = False
             user.save()
             activateEmail(request, user, form.cleaned_data.get('email'))
-            return redirect("Home:index")
+            return JsonResponse({"success": True, "message": "Registration successful. Please check your email to activate your account."}, status=200)
+        else:
+            print("111111111")
+            return JsonResponse({"success": False, "message": "Invalid form data."}, status=HttpResponseBadRequest.status_code)
     else:
-        form = UserCreationFormWithEmail()
-
-    return render(request, "accounts/register.html", {'form': form})
+        print("22222222222")
+        return JsonResponse({"success": False, "message": "Invalid request method."}, status=HttpResponseBadRequest.status_code)
