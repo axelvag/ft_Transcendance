c4f9243cdf58781e835293d9b0ccf05d003edc34
axelvag <vaganay.axel@gmail.com>
2024-01-24 15:46:57 +0100

    [#51][fix_api_gateway] 127.0.0.1:5000/authentification work

--- api_gateway/api_gateway.py
+++ api_gateway/api_gateway.py
@@ -3,6 +3,7 @@ from werkzeug.exceptions import HTTPException
 import logging
 import yaml
 import requests
+import base64
 
 app = Flask(__name__)
 
@@ -29,15 +30,13 @@ def redirect_to_authentication():
 
 def get_service_url(route):
     service_url = routing_config.get(route)
-    # service_url = "http://127.0.0.1:8002/pages"
-    # if not service_url:
-    #     app.logger.error(f"No service found for route: {route}")
-    #     return None
+
+    # Modifiez cette ligne pour utiliser le nom du service
+    # service_url = "http://transcendance-authentification-1:8001"
 
     app.logger.debug(f"Route: {route}, Service URL: {service_url}")
     return service_url
 
-# Ajoutez une logique pour gérer la redirection dans forward_request
 def forward_request(service_url, route, request):
     # Ne pas ajouter "/profile" à la fin de l'URL
     forward_url = f"{service_url}"
@@ -60,61 +59,17 @@ def forward_request(service_url, route, request):
             params=request.args,
         )
 
-        # Ajoutez une logique pour suivre la redirection si nécessaire
-        if response.status_code == 301 or response.status_code == 302:
-            location = response.headers.get('Location')
-            if location:
-                forward_url = location
-                app.logger.debug(f"4 Following redirection to: {forward_url}")
-                # Refaites la demande avec la nouvelle URL
-                response = requests.request(
-                    method=request.method,
-                    url=forward_url,
-                    headers=request.headers,
-                    data=request.get_data(),
-                    params=request.args,
-                )
-
         app.logger.debug(f"333333333333333333 {response}")
-        return response.content, response.status_code, response.headers.items()
+
+        # Convertir les données de type bytes en str pour jsonify
+        # Pour fixe une erreur "TypeError: Object of type bytes is not JSON serializable"
+        response_data = response.content.decode('utf-8')
+
+        return response_data, response.status_code, response.headers.items()
     except requests.RequestException as e:
         abort(500, f"Failed to forward request to {forward_url} : {str(e)}")
+00, f"Failed to forward request to {forward_url} : {str(e)}")
 
-
-# def forward_request(service_url, route, request):
-#     # Ne pas ajouter "/profile" à la fin de l'URL
-#     forward_url = f"{service_url}"
-
-#     # app.logger.debug(f"SERVICE URL {service_url}")
-#     # app.logger.debug(f"ROUTE {route}")
-#     # app.logger.debug(f"REQUEST.PATH {request.path}")
-#     app.logger.debug(f"Forwarding request to {forward_url}")
-#     app.logger.debug(f"22222222222222222")
-
-#     try:
-#         app.logger.debug(f"request.Methode {request.method}")
-#         app.logger.debug(f"forward_url {forward_url}")
-#         app.logger.debug(f"request.headers {request.headers}")
-#         app.logger.debug(f"request.get_data() {request.get_data()}")
-#         app.logger.debug(f"request.args {request.args}")
-
-#         response = requests.request(
-#             method=request.method,
-#             url=forward_url,
-#             headers=request.headers,
-#             data=request.get_data(),
-#             params=request.args,
-#         )
-#         app.logger.debug(f"333333333333333333 {response}")
-#         return response.content, response.status_code, response.headers.items()
-#     except requests.RequestException as e:
-#         # app.logger.exception("An error occurred during request forwarding:")
-#         # app.logger.debug(f"method {method}")
-#         # app.logger.debug(f"url {url}")
-#         # app.logger.debug(f"headers {headers}")
-#         # app.logger.debug(f"data {data}")
-#         # app.logger.debug(f"params {params}")
-#         abort(500, f"Failed to forward request to {forward_url} : {str(e)}")
 
 @app.route('/<path:route>', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def proxy(route):
@@ -138,48 +93,3 @@ def proxy(route):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
-
-
-
-
-# # api_gateway.py
-# from flask import Flask, request
-# from werkzeug.exceptions import HTTPException
-# import logging
-# import yaml
-
-# # import os
-# # # Imprimez le répertoire de travail actuel
-# # print("Current working directory:", os.getcwd())
-
-# # Creer l'API Flask
-# app = Flask(__name__)
-
-# # Chargement des fichiers de config
-# with open('/app/api_gateway/config/routing.yaml', 'r') as file:
-#     routing_config = yaml.safe_load(file)
-
-# # with open('/authentication.yaml', 'r') as file:
-# #     authentication_config = yaml.safe_load(file)
-
-# with open('/app/api_gateway/config/rate_limiting.yaml', 'r') as file:
-#     rate_limiting_config = yaml.safe_load(file)
-
-# # Application des middlewares
-# from middleware import logging, error_handling
-
-# # Enregistrement des blueprints (modele) pour les middlewares
-# app.register_blueprint(logging.logging_bp)
-# app.register_error_handler(HTTPException, error_handling.handle_http_exception)
-
-# # Define route for each service based on routing configuration
-# for route, service_url in routing_config.items():
-#     endpoint_name = f"proxy_{route}"  # Utilisez un nom d'endpoint unique pour chaque route
-#     @app.route(route, methods=['GET', 'POST', 'PUT', 'DELETE'], endpoint=endpoint_name)
-#     def proxy(route=route, service_url=service_url):
-#         # Implement logic to forward requests to the appropriate service
-#         # This can involve authentication, rate limiting, etc.
-#         pass
-
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=5000, debug=True)
