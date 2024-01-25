9fdde6aac1bd1543b9e6f1cfc161bf3a2c092456
axelvag <vaganay.axel@gmail.com>
2024-01-25 16:08:00 +0100

    [#51][fix_api_gateway] 8000:80 redirect to front/public/index.html and SPA work

--- front/public/views/home.js
+++ front/public/views/home.js
@@ -1,10 +1,38 @@
 const template = `
-    <div class="layout">
-        <main>
-            <h1>Home</h1>
-            <p>Welcome to your profile page. Here you can view and edit your profile information.</p>
-        </main>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="./css/home_auth.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>django auth</title>
+</head>
+<body>
+  <div class="navbar">
+    <img src="assets/img/logoTranscendencenew.png" alt="Description de l'image" class="img-home">
+    <div class="flex">
+      <label class="switch">
+        <input type="checkbox" id="toggle-example" onchange="toggleTheme()">
+        <span class="slider round"></span>
+      </label>
+        <p class="margin_left"><a data-link="/login" class="play-now-btn">Connexion</a></p>
+        <p class="margin_left"><a data-link="/profil" class="play-now-btn">Profil</a></p>
+        <p class="margin_left"><a data-link="/signup" class="play-now-btn">Inscription</a></p>
     </div>
+  </div>
+  <div class="bottom_navbar"></div>
+    <div class="padding-global">
+      <div class="center-container">
+        <h1 class="gradient-text-home">The Pong Game</h1>
+        <div class="margin_top"></div>
+        <form action="#">
+          <button type="submit" class="play-now-btn_play">Play Now</button>
+        </form>
+      </div>
+      <script src="{% static 'js/theme-toggle.js' %}"></script>
+    </div>
+</body>
+</html>
 `;
 
 
--- front/public/views/view-signup.ce.js
+++ front/public/views/view-signup.ce.js
@@ -1,10 +1,9 @@
 import '../components/layouts/auth-layout.ce.js';
-import { redirectTo } from '../router.js';
 
 class ViewSigUp extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-      <login-layout>
+        <login-layout>
         <h1 class="fw-bold py-2 mb-4">
           <span class="text-gradient">Sign up</span>
         </h1>
@@ -22,16 +21,16 @@ class ViewSigUp extends HTMLElement {
             <input class="form-control form-control-lg" type="username" id="username" name="username" required />
           </div>
           <div class="mb-4">
-            <label class="form-label opacity-50" for="password">
+            <label class="form-label opacity-50" for="password1">
               Choose your password
             </label>
-            <input class="form-control form-control-lg" type="password" id="password" name="password" required />
+            <input class="form-control form-control-lg" type="password" id="password1" name="password1" required />
           </div>
           <div class="mb-4">
-            <label class="form-label opacity-50" for="password">
+            <label class="form-label opacity-50" for="password2">
               Repeat your password
             </label>
-            <input class="form-control form-control-lg" type="password" id="password" name="password" required />
+            <input class="form-control form-control-lg" type="password" id="password2" name="password2" required />
           </div>
           <div class="d-grid pt-3">
             <button type="submit" class="btn btn-primary btn-lg fw-bold">
@@ -50,48 +49,69 @@ class ViewSigUp extends HTMLElement {
     this.querySelector('#signup-form').addEventListener('submit', this.submitForm.bind(this));
   }
 
-  submitForm(event) {
-    console.log('Click submit !');
+  async submitForm(event) {
+    console.log("Click submit !");
     event.preventDefault();
-    // const form = event.target;
-    // const formData = new FormData(form);
+    const form = event.target;
+    const username = document.getElementById("username").value;
+    const email = document.getElementById("email").value;
+    const password1 = document.getElementById("password1").value;
+    const password2 = document.getElementById("password2").value;
+    const formData = {
+      username: username,
+      email: email,
+      password1: password1,
+      password2: password2,
+  };
+    console.log(JSON.stringify(formData));
+    //to do
     // const csrfToken = this.getCSRFToken();
-
-    setTimeout(() => {
-      console.log('Test');
-      redirectTo('/profil');
-    }, 2000);
+    // const response = await fetch(
+    //   "http://127.0.0.1:8000/accounts/get-csrf-token/",
+    //   {
+      //       method: "GET",
+    //       credentials: "include",
+    //   }
+    // )
+    // console.log('response', response);
+    // const data = await response.json();
+    // const csrfToken = data.csrfToken;
+    // .then((response) => response.json())
+    // .then((data) => {
+      //     const csrfToken = data.csrfToken;
+      //     console.log(csrfToken)
+      
+      // Perform the AJAX request
+      // console.log(csrfToken)
+    const response = await fetch("http://127.0.0.1:8000/accounts/register/", {
+        method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+        // 'X-CSRFToken': csrfToken
+      },
+      credentials: "include",
+      body: JSON.stringify(formData),
+    })
+    const data =  await response.json();
+    if (data.success) {
+        alert('success');
+    } else {
+      alert('errors');
+      console.log(data.errors);
+    }
   }
+
+  // getCSRFToken() {
+  //   const cookies = document.cookie.split(';');
+  //   for (let i = 0; i < cookies.length; i++) {
+  //     let cookie = cookies[i].trim();
+  //     if (cookie.startsWith('csrftoken=')) {
+  //       return cookie.substring('csrftoken='.length);
+  //     }
+  //   }
+  //   return '';
+  // }
+  
 }
 
 customElements.define('view-signup', ViewSigUp);
-
-// Perform the AJAX request
-// fetch('http://127.0.0.1:8000/accounts/register/', {
-//   method: 'POST',
-//   body: formData,
-//   headers: {
-//     'X-CSRFToken': csrfToken,
-//   },
-// })
-//   .then(response => response.json())
-//   .then(data => {
-//     if (data.success) {
-//       console.log('Registration successful:', data.message);
-//       redirectTo('/profile');
-//     } else console.error('Registration failed:', data.message);
-//   })
-//   .catch(error => {
-//     console.error('Error during fetch:', error);
-//   });
-
-// getCSRFToken() {
-//   const cookies = document.cookie.split(';');
-//   for (let i = 0; i < cookies.length; i++) {
-//     let cookie = cookies[i].trim();
-//     if (cookie.startsWith('csrftoken=')) {
-//       return cookie.substring('csrftoken='.length);
-//     }
-//   }
-//   return '';
-// }
