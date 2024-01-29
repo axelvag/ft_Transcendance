860dbc8f58dd82d6aab1cbab793abd2834d21129
Lucas-Ve <lveloso@student.42lyon.fr>
2024-01-29 15:59:06 +0100

    [#58][login_front_to_back] email validation finish & email resend inprogress

--- front/public/views/view-email-confirmation.ce.js
+++ front/public/views/view-email-confirmation.ce.js
@@ -25,33 +25,27 @@ class ViewEmailConfirmation extends HTMLElement {
 
       </login-layout>
     `;
-    this.hasBeenInitialized = true; // Marquer comme initialisé
-    // const params = new URL(document.location).searchParams;
     console.log(document.location.href);
-    // const uidb64 = params.get("uidb64");
-    // const token = params.get("token");
-    // console.log(uidb64);
-    // console.log(token);
     const hash = location.hash;
-    // Isole la partie de requête du fragment - supprimez tout jusqu'au '?'
     const queryString = hash.slice(hash.indexOf('?') + 1);
-    // Utilise URLSearchParams pour analyser la partie de requête
     const params = new URLSearchParams(queryString);
-    // Récupère les valeurs de uidb64 et token
     const uidb64 = params.get('uidb64');
     const token = params.get('token');
     console.log(uidb64);
     console.log(token);
-    // const response1 = await fetch(`http://127.0.0.1:8000/accounts/is_user_active/${uidb64}/${token}`);
-    // const data1 = await response1.json();
-    // if (data1.success) {
-    //   alert('success');
-    // } else {
-    //   alert('errors');
+
+    const response1 = await fetch(`http://127.0.0.1:8000/accounts/is_user_active/${uidb64}/${token}`);
+    const data1 = await response1.json();
+    if (data1.success) {
+      // if (data.message) this.querySelector('#email-confirm-success').textContent = data.message;
+      console.log("user Actif");
+      this.querySelector('#email-confirm-loading').hidden = true;
+      this.querySelector('#email-confirm-success').hidden = false;
+    }
+    else {
       const response = await fetch(`http://127.0.0.1:8000/accounts/activate/${uidb64}/${token}`);
       const data = await response.json();
       console.log(data);
-  
       this.querySelector('#email-confirm-loading').hidden = true;
       if (data.success) {
         // if (data.message) this.querySelector('#email-confirm-success').textContent = data.message;
@@ -62,10 +56,7 @@ class ViewEmailConfirmation extends HTMLElement {
         }
         this.querySelector('#email-confirm-error').hidden = false;
       }
-      console.log(data1.errors);
-    // }
+    }
   }
-  
 }
-
 customElements.define('view-email-confirmation', ViewEmailConfirmation);
