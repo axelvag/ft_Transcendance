ebe5b15b4e848043a2057dc5b92d0ddfbbdae800
axelvag <vaganay.axel@gmail.com>
2024-01-30 15:56:56 +0100

    [#64][web_socket] arret propre du server avec kill du port -> server.js

--- communication/server.js
+++ communication/server.js
@@ -1,30 +1,47 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+// Importation des modules
+var express = require('express');
+var http = require('http');
+var io = require('socket.io');
+
+// Création de l'application Express
+var app = express();
+
+// Création du serveur HTTP en utilisant le module http
+var server = http.createServer(app);
+
+// Configuration de Socket.IO en utilisant le serveur HTTP créé ci-dessus
+var socketServer = io(server);
 
 // Configuration de la route pour la page d'accueil
-// Et envoie du fichier HTML de la page d'accueil
 app.get("/", function(req, res){
   res.sendFile(__dirname + '/index.html');
-})
+});
 
 // Gestion des connexions WebSocket
-io.on('connection', function(socket){
+socketServer.on('connection', function(socket){
   console.log('a user is connected');
+  
   socket.on('disconnect', function(){
     console.log('a user is disconnect');
-  })
+  });
+
   socket.on('chat message', function(msg){
-    // Affichage du message reçu dans la console du serveur
     console.log('message recu : ' + msg);
-    // Émission du message à tous les clients connectés
-    io.emit('chat message', msg)
-  })
+    socketServer.emit('chat message', msg);
+  });
+});
 
+// Démarrage du serveur sur le port 3000
+var listener = server.listen(3000, function(){
+  console.log("Server running on 3000");
+});
 
-
-})
-
-http.listen(3000, function(){
-  console.log("Server runing on 3000")
-})
+// Gestionnaire pour l'arrêt propre du serveur
+process.on('SIGINT', () => {
+  console.log('Arrêt du serveur...');
+  // listener.close(() => {
+  //   console.log('Serveur arrêté.');
+  //   process.exit();
+  // });
+  process.exit();
+});
