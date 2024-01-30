1a9761e7df33137dd9601bcc73195c31712e2fe8
ascatama@student.42lyon.fr <ascatama@student.42lyon.fr>
2024-01-30 15:34:18 +0100

    PR

--- front/public/views/view-signup.ce.js
+++ front/public/views/view-signup.ce.js
@@ -1,9 +1,9 @@
 import '../components/layouts/auth-layout.ce.js';
 
 class ViewSigUp extends HTMLElement {
-  
-  constructor(){
+  constructor() {
     super();
+    this.submitForm = this.submitForm.bind(this);
   }
   connectedCallback() {
     this.innerHTML = `
@@ -29,11 +29,11 @@ class ViewSigUp extends HTMLElement {
             
             <div class="mb-4">
               <label class="form-label opacity-50" for="password1">Choose your password</label>
-              <input class="form-control form-control-lg" type="password" id="password1" name="password1" required />
+              <input class="form-control form-control-lg" type="password" id="password1" name="password1" value="Qwertyuiop123." required />
             </div>
             <div class="mb-4">
               <label class="form-label opacity-50" for="password2">Repeat your password</label>
-              <input class="form-control form-control-lg" type="password" id="password2" name="password2" required />
+              <input class="form-control form-control-lg" type="password" id="password2" name="password2" value="Qwertyuiop123." required />
               <div id="password-error" class="invalid-feedback"></div>
             </div>
             
@@ -55,7 +55,7 @@ class ViewSigUp extends HTMLElement {
             </form>
       </login-layout>
     `;
-    
+
     //For function
     this.passwordVerification = this.passwordVerification.bind(this);
     this.resetError = this.resetError.bind(this);
@@ -71,60 +71,54 @@ class ViewSigUp extends HTMLElement {
 
     //Event
     // this.displayFormErrors = this.displayFormErrors.bind(this);
-    this.querySelector('#signup-form').addEventListener('submit', this.submitForm.bind(this));
+    this.querySelector('#signup-form').addEventListener('submit', this.submitForm);
+    this.querySelector('#signup-form').addEventListener('click', e => e.stopPropagation());
+  }
+
+  disconnectedCallback() {
+    this.querySelector('#signup-form').removeEventListener('submit', this.submitForm);
   }
 
   passwordVerification = () => {
-  
     if (this.password1.value !== this.password2.value) {
       this.passwordError.textContent = 'The passwords do not match.';
       this.password1.classList.add('is-invalid');
       this.password2.classList.add('is-invalid');
-      // this.passwordError.style.display = 'block';
-      console.log("password !=");
+      console.log('password !=');
       return false;
-    } 
-    
+    }
+
     const minLength = 8;
     const Uppercase = /[A-Z]/.test(this.password1.value);
     const Lowercase = /[a-z]/.test(this.password1.value);
     const Number = /\d/.test(this.password1.value);
     const SpecialChar = /[^A-Za-z0-9]/.test(this.password1.value);
 
-    if (this.password1.length < minLength || !Uppercase || !Lowercase || !Number || !SpecialChar){
-      console.log("Je rentre ici");
-      this.passwordError.textContent = 'The password must contain at least 8 characters, an upper case letter, a lower case letter, a number and a special character.';
+    if (this.password1.length < minLength || !Uppercase || !Lowercase || !Number || !SpecialChar) {
+      this.passwordError.textContent =
+        'The password must contain at least 8 characters, an upper case letter, a lower case letter, a number and a special character.';
       this.password1.classList.add('is-invalid');
       this.passwordError.style.display = 'block';
       return false;
     }
 
     return true;
-  }
-  
+  };
 
   async submitForm(event) {
-
-    console.log('Click submit !');
     event.preventDefault();
 
     this.resetError();
 
     let verif = this.passwordVerification();
-    if (!verif){
-      console.log("YEPPPPP");
-      return;
-    }
+    if (!verif) return;
 
-    console.log("toto");
     const formData = {
       username: this.username.value,
       email: this.email.value,
       password1: this.password1.value,
       password2: this.password2.value,
     };
-
-    console.log(JSON.stringify(formData));
 
     const response = await fetch('http://127.0.0.1:8000/accounts/register/', {
       method: 'POST',
@@ -136,100 +130,33 @@ class ViewSigUp extends HTMLElement {
       body: JSON.stringify(formData),
     });
 
-      // const data = await response.json();
-      // // console.log("test");
-      // if (response.ok){
-      //   const successNotification = document.getElementById('success-notification');
-      //   console.log("var-->", successNotification);
-      //   if (successNotification)
-      //     successNotification.style.display = 'block';
-      // }
-      // else if (data.errors.email){
-      //   this.emailError.textContent = data.errors.email[0];
-      //   this.email.classList.add('is-invalid');
-      // }
-      // else if (data.errors.password2){
-      //   this.usernameError.textContent = data.errors.password2[0];
-      //   this.username.classList.add('is-valid');
-      // }
-    const data = await response.json(); 
-    console.log("data", data);
+    const data = await response.json();
 
-    if (data.errors.email){
-        console.log("pussy");
-      this.emailError.textContent = data.errors.email[0];
-      this.email.classList.add('is-invalid');
-    }
-    else if (data.errors.password2){
-        console.log("puddwdwdwdwssy");
-        this.usernameError.textContent = data.errors.password2[0];
+    if (data.success) {
+      const successNotification = document.getElementById('success-notification');
+      if (successNotification) successNotification.style.display = 'block';
+    } 
+    else {
+      if (data.errors.email) {
+        this.emailError.textContent = data.errors.email[0];
+        this.email.classList.add('is-invalid');
+      } else if (data.errors.password2) {
+        this.usernameError.textContent = data.error.password2[0];
         this.username.classList.add('is-valid');
-    }
-    else{
-        console.log("Formulaire --> Sucess");
-        const successNotification = document.getElementById('success-notification');
-        // successNotification.classList.remove('alert-success');
-        console.log("var-->", successNotification);
-        if (successNotification)
-          successNotification.style.display = 'block';
-    }
-    
-    }
-    
-    resetError = () => {
-      this.email.classList.remove('is-invalid');
-      this.password1.classList.remove('is-invalid');
-      this.password2.classList.remove('is-invalid');
-      this.usernameError.classList.remove('is-valid');
-
-      this.emailError.textContent = '';
-      this.usernameError.textContent = '';
-      this.passwordError.textContent = '';
+      }
     }
   }
 
-  customElements.define('view-signup', ViewSigUp);
+  resetError = () => {
+    this.email.classList.remove('is-invalid');
+    this.password1.classList.remove('is-invalid');
+    this.password2.classList.remove('is-invalid');
+    this.usernameError.classList.remove('is-valid');
 
-  
-  //to do
-  // [x] Gestions des mots de passes (Les deux egaux)
-// [x] Verifier que l'adresse mail n'est pas utilise
-// [x] Verifier que l'adress mail contient bien un @
-// [x] Tous les champs sont rempli
+    this.emailError.textContent = '';
+    this.usernameError.textContent = '';
+    this.passwordError.textContent = '';
+  };
+}
 
-// [x] Verifier que le mot de passe contient : 8 caractere, 1 lettre minuscule, 1 lettre majuscule, et un carcatere special et un chiffre
-//       - 8 caractere
-//       - 1 lettre minuscule
-//       - 1 lettre majuscule
-//       - un carcatere special
-//       - un chiffre
-
-// [x] Gerer erreur mdp errone plus juste mais mauvaise adress mail, le message d'erreur des mdp est encore la
-// [x!] Gerer quand ca sucess de mettre un message comme quoi ca a marche
-// [x!] Mettre un message check your emai
-// [] Si form a des erreurs avant, et que ensuite ca sucess, le message de sucess ne s'affiche pas
-
-
-// if (data.errors) {
-//   Object.keys(data.errors).forEach((key) => {
-  //     console.log("key ", key);
-  //     const errorElement = this.querySelector(`#${key}-error`);
-  //     if (errorElement) {
-    //       errorElement.textContent = data.errors[key][0];
-    //       this[key].classList.add('is-invalid');
-    //     }
-    //   });
-    // } else {
-      //   // Afficher le message de succès si aucune erreur n'est retournée
-      //   const successNotification = document.getElementById('success-notification');
-      //   if (successNotification) successNotification.style.display = 'block';
-      // }
-
-      // ['email', 'password1', 'password2', 'username'].forEach((field) => {
-      //   const input = this[field];
-      //   const errorElement = this.querySelector(`#${field}-error`);
-      //   if (input) input.classList.remove('is-invalid');
-      //   if (errorElement) errorElement.textContent = '';
-      // });
-
-//Qwertyuiop123.
+customElements.define('view-signup', ViewSigUp);
