01ca117ea65f5f99225a94542cdf08a38841fb69
axelvag <vaganay.axel@gmail.com>
2024-01-30 14:36:55 +0100

    [#64][web_socket] comment websocket

--- communication/server.js
+++ communication/server.js
@@ -2,17 +2,22 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+// Configuration de la route pour la page d'accueil
+// Et envoie du fichier HTML de la page d'accueil
 app.get("/", function(req, res){
   res.sendFile(__dirname + '/index.html');
 })
 
+// Gestion des connexions WebSocket
 io.on('connection', function(socket){
   console.log('a user is connected');
   socket.on('disconnect', function(){
     console.log('a user is disconnect');
   })
   socket.on('chat message', function(msg){
+    // Affichage du message reçu dans la console du serveur
     console.log('message recu : ' + msg);
+    // Émission du message à tous les clients connectés
     io.emit('chat message', msg)
   })
 
