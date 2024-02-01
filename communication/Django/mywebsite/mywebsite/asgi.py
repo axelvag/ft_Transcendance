ea8aa3c03d1828c9984297acacfd83a34e7e8628
axelvag <vaganay.axel@gmail.com>
2024-02-01 15:27:00 +0100

    [#76][web_socket_django] complete chat live channel, finish video but error /ws/socket-server

--- communication/Django/mywebsite/mywebsite/asgi.py
+++ communication/Django/mywebsite/mywebsite/asgi.py
@@ -4,13 +4,23 @@ ASGI config for mywebsite project.
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mywebsite.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    'http':get_asgi_application(),
+    'websocket':AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    )
+})
