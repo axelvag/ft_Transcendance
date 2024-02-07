075df52aa4ed763ced974b106385c0ff3d002bab
ascatama@student.42lyon.fr <ascatama@student.42lyon.fr>
2024-02-07 13:47:50 +0100

    [back][#81] Send inviattion ok and receive base of donne django

--- friendship/friendshipApp/invitations/admin.py
+++ friendship/friendshipApp/invitations/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# from django.contrib import admin
+from .models import Invitation
+
+@admin.register(Invitation) # enregistre dans le systeme d'administration django
+class InvitationAdmin(admin.ModelAdmin):
+    list_display = ('from_user', 'to_email', 'accepted')
