ca287c50c876df71f63fb39cc8e4f3e79e709f6b
ascatama@student.42lyon.fr <ascatama@student.42lyon.fr>
2024-02-14 11:48:17 +0100

    [back][#101] Error connect bdd PostgreSQL

--- friendship/friendshipApp/friendshipApp/settings.py
+++ friendship/friendshipApp/friendshipApp/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -64,6 +66,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'invitations',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -103,14 +106,14 @@ WSGI_APPLICATION = 'friendshipApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        # 'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': os.getenv('POSTGRES_DB'),  # Lecture de la variable d'environnement sans valeur par défaut
-        # 'USER': os.getenv('POSTGRES_USER'),
-        # 'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        # 'HOST': os.getenv('POSTGRES_HOST'),
-        # 'PORT': '5432',  # Le port par défaut de PostgreSQL
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),  # Lecture de la variable d'environnement sans valeur par défaut
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': '5432',  # Le port par défaut de PostgreSQL
     }
 }
 
