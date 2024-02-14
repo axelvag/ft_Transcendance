449eced9e3cece1e61c496616affabb358956be1
ascatama@student.42lyon.fr <ascatama@student.42lyon.fr>
2024-02-14 11:48:48 +0100

    [back][#110] Error connect bdd PostgreSQL

--- friendship/friendshipApp/friendshipApp/settings.py
+++ friendship/friendshipApp/friendshipApp/settings.py
@@ -113,7 +113,7 @@ DATABASES = {
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': os.getenv('POSTGRES_HOST'),
-        'PORT': '5432',  # Le port par défaut de PostgreSQL
+        'PORT': '5432',  # Le port par défaut de PostgreSQLd
     }
 }
 
