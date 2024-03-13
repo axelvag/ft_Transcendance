3d377bb8326d5bed96fe0f6c0bd172456e3ed77b
axelvag <vaganay.axel@gmail.com>
2024-03-13 14:01:39 +0100

    [#171][stat_back] 8004/statistic accessible

--- statistic/statback/statback/urls.py
+++ statistic/statback/statback/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pages/', include('pages.urls')),
-    # path('statistic/', include('statistic.urls')),
+    path('statistic/', include('statistic.urls')),
 ]
