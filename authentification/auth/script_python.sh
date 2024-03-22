fdad1397ee30bdcbb1ee2d71f0b273c79fefbff2
Axel Vaganay <avaganay@z2r1p6.42lyon.fr>
2024-03-22 15:50:58 +0100

    try to fix migration, I copy /game for /profile but error /manage.py don't find

--- authentification/auth/script_python.sh
+++ authentification/auth/script_python.sh
@@ -2,13 +2,13 @@
 
 
 # Fonction pour attendre que la base de données soit prête
-wait_for_db() {
-    # Appeler wait-for-it.sh pour attendre que la base de données soit disponible sur le port 5432
-    /wait-for-it.sh -t 60 data_base:5432 -- echo "La base de données est prête"
-}
+# wait_for_db() {
+#     # Appeler wait-for-it.sh pour attendre que la base de données soit disponible sur le port 5432
+#     /wait-for-it.sh -t 60 data_base:5432 -- echo "La base de données est prête"
+# }
 
-# Attendre que la base de données soit disponible
-wait_for_dbsh -t 60 data_base:5432 -- echo "La base de données est prête"
+# # Attendre que la base de données soit disponible
+# wait_for_dbsh -t 60 data_base:5432 -- echo "La base de données est prête"
 
 # sleep 120
 
--- migrate.sh
+++ migrate.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# docker compose run --rm authentification python manage.py makemigrations
-# docker compose run --rm authentification python manage.py migrate
+docker compose run --rm authentification python manage.py makemigrations
+docker compose run --rm authentification python manage.py migrate
 
-# docker stop authentification
-# docker start authentification
-# docker stop friendship
-# docker start friendship
-# docker stop profile
-# docker start profile
+docker stop authentification
+docker start authentification
+docker stop friendship
+docker start friendship
+docker stop profile
+docker start profile
