9290dd364cd5d6f44aa728e5855ae3cf05550db5
Axel Vaganay <avaganay@z1r3p5.42lyon.fr>
2024-03-28 15:54:53 +0100

    start gunicorn

--- profile/profileApp/profileApp/settings.py
+++ profile/profileApp/profileApp/settings.py
@@ -147,16 +147,17 @@ EMAIL_HOST_USER = 'transcendence42lyon@gmail.com'
 EMAIL_HOST_PASSWORD = 'zotbqnassvkvftvk'
 
 
-CORS_ALLOWED_ORIGINS = [
-    f"{URL}:5500",  # Remplacez par l'URL de votre frontend
-    f"{URL}:8000",
-]
 
 # CORS_ORIGINS_ALLOWED_ALL = True
 
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = [
+#     f"{URL}:5500",  # Remplacez par l'URL de votre frontend
+#     f"{URL}:8000",
+# ]
+CORS_ALLOWED_ORIGINS = ['https://*']
 
 ALLOWED_HOSTS=['*']
 
