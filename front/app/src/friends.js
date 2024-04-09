c129e2beb5e4ce166d59e9f1cc26f50b89d008d5
Alessio Scatamacchia <ascatama@z2r1p7.42lyon.fr>
2024-04-09 14:52:44 +0200

    temprary

--- front/app/src/friends.js
+++ front/app/src/friends.js
@@ -16,35 +16,37 @@ const initWebSocket = () => {
     websocketInstance.onopen = function (event) {
       console.log('WebSocket connection established:', event);
     };
-    
-    websocketInstance.onmessage = function(event) {
+
+    websocketInstance.onmessage = function (event) {
       console.log('WebSocket message received:', event);
       const data = JSON.parse(event.data);
-  
+
       // Assurer que data.id existe et est défini avant de continuer
       if (data && data.id !== undefined) {
-          // Récupérer les notifications déjà reçues du localStorage
-          let receivedNotifications = localStorage.getItem('receivedNotifications');
-          receivedNotifications = receivedNotifications ? JSON.parse(receivedNotifications) : [];
-  
-          // Convertir data.id en chaîne pour la comparaison et le stockage cohérent
-          const dataIdStr = data.id.toString();
-  
-          // Vérifier si la notification a déjà été reçue
-          if (!receivedNotifications.includes(dataIdStr)) {
-              if (data.message) {
-                  notify(data.message); // Afficher la notification
-              }
-  
-              // Ajouter l'ID de la notification aux notifications déjà reçues et mettre à jour le localStorage
-              receivedNotifications.push(dataIdStr);
-              localStorage.setItem('receivedNotifications', JSON.stringify(receivedNotifications));
-          }
+        // Récupérer les notifications déjà reçues du localStorage
+        // let receivedNotifications = localStorage.getItem('receivedNotifications');
+        // receivedNotifications = receivedNotifications ? JSON.parse(receivedNotifications) : [];
+
+        // // Convertir data.id en chaîne pour la comparaison et le stockage cohérent
+        // const dataIdStr = data.id.toString();
+        // console.log("dataIdSTR", dataIdStr);
+
+        // // Vérifier si la notification a déjà été reçue
+        // console.log("message566666666666666666666666", receivedNotifications);
+        // if (!receivedNotifications.includes(dataIdStr)) {
+        if (data.message) {
+          notify(data.message); // Afficher la notification
+          // }
+
+          // // Ajouter l'ID de la notification aux notifications déjà reçues et mettre à jour le localStorage
+          // receivedNotifications.push(dataIdStr);
+          // localStorage.setItem('receivedNotifications', JSON.stringify(receivedNotifications));
+        }
       } else {
-          console.log('Received data is missing an id:', data);
+        console.log('Received data is missing an id:', data);
       }
-  };
-  
+    };
+
 
     websocketInstance.onerror = function (event) {
       console.error('WebSocket error observed:', event);
