432779ab9110de8ae781b66e68e890b2709cfbb1
davfront <contact@david-pereira.net>
2024-04-28 11:39:45 +0200

    [#227][fix] use env vars from .env in front

--- front/app/src/constants.js
+++ front/app/src/constants.js
@@ -1,5 +1,5 @@
-export const BASE_URL = 'http://127.0.0.1';
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
 
-export const WS_BASE_URL = 'ws://127.0.0.1';
+export const WS_BASE_URL = import.meta.env.VITE_WS_BASE_URL;
 
-export const OAUTH_AUTHORIZE_URL = 'https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-032700fdff8bf6b743669184234c5670698f0f0ef95b498514fc13b5e7af32f0&redirect_uri=http%3A%2F%2F127.0.0.1%3A8000%2Fauth42-callback&response_type=code';
+export const OAUTH_AUTHORIZE_URL = import.meta.env.VITE_OAUTH_AUTHORIZE_URL;
