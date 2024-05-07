768591cede79fe1076d77e6ad0b6bc43e64200b4
davfront <contact@david-pereira.net>
2024-05-07 23:37:54 +0200

    [front] fix hot reload on dev mode

--- front/app/vite.config.js
+++ front/app/vite.config.js
@@ -26,9 +26,12 @@ export default defineConfig({
   server: {
     host: true,
     port: '8000',
-    https: process.env.NODE_ENV === 'development' && {
+    https: {
       key: fs.readFileSync('/etc/ssl/private/nginx-selfsigned.key'),
       cert: fs.readFileSync('/etc/ssl/certs/nginx-selfsigned.crt'),
+    },
+    watch: {
+      usePolling: true,
     },
   },
 });
