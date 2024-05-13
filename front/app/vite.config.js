b50af4de3b9a2dfbe864f39b95cbc8827cefbdbb
dapereir <dapereir@student.42lyon.fr>
2024-05-13 14:16:56 +0200

    [front] fix ssl file error

--- front/app/vite.config.js
+++ front/app/vite.config.js
@@ -26,7 +26,7 @@ export default defineConfig({
   server: {
     host: true,
     port: '8000',
-    https: {
+    https: process.env.NODE_ENV === 'development' && {
       key: fs.readFileSync('/etc/ssl/private/nginx-selfsigned.key'),
       cert: fs.readFileSync('/etc/ssl/certs/nginx-selfsigned.crt'),
     },
