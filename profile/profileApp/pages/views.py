121c1966ad216c51398209042b3f25ce7afcbb57
Alessio Scatamacchia <ascatama@z2r1p7.42lyon.fr>
2024-05-13 14:19:44 +0200

    [security][#protect_request_api] clean service back profile and protect the requests call

--- profile/profileApp/pages/views.py
+++ profile/profileApp/pages/views.py
@@ -21,42 +21,41 @@ def verif_sessionID(view_func):
     def wrapper(request, *args, **kwargs):
         session_id = request.COOKIES.get('sessionid', None)
         update_url = f"https://authentification:8001/accounts/verif_sessionid/{session_id}"
-        response = requests.get(update_url, verify=False)
 
-        if response.status_code != 200:
-            return JsonResponse({"success": False, "message": "SessionID Invalid"}, status=400)
+        try:
+            response = requests.get(update_url, verify=False)
+            if response.status_code != 200:
+                return JsonResponse({"success": False, "message": "SessionID Invalid"}, status=400)
+        except requests.exceptions.RequestException as e:
+            return JsonResponse({"success": False, "message": "Network Error"}, status=500)
 
-        # Si la vérification est réussie, exécuter la vue originale
         return view_func(request, *args, **kwargs)
 
     return wrapper
+
 
 @csrf_exempt
 @verif_sessionID
 @require_http_methods(["POST"])
 def update_user(request):
-    if request.content_type.startswith('multipart/form-data'): # grace au formData envoyer
-        # Extraction des données depuis une requête multipart/form-data
+    if request.content_type.startswith('multipart/form-data'):
         user_id = request.POST.get('id')
         first_name = request.POST.get('firstname', '')
         last_name = request.POST.get('lastname', '')
         username = request.POST.get('username', '')
-        # email = request.POST.get('email', '')
         avatar = request.FILES.get('avatar', None)
         if avatar is None:
             avatar42 = request.POST.get('avatar', None)
         else:
             avatar42 = None
     else:
-        # Tentative d'extraction des données JSON
         try:
             data = json.loads(request.body.decode('utf-8'))
             user_id = data.get('id')
             first_name = data.get('firstname', '')
             last_name = data.get('lastname', '')
             username = data.get('username', '')
-            # email = data.get('email', '')
-            avatar = request.FILES.get('avatar', None)  # Pas d'avatar dans les données JSON
+            avatar = request.FILES.get('avatar', None)
             if avatar is None:
                 avatar42 = data.get('avatar', None)
             else:
@@ -64,9 +63,8 @@ def update_user(request):
         except json.JSONDecodeError:
             return JsonResponse({"success": False, "message": "Invalid or missing JSON data."}, status=400)
 
-    # Mise à jour des informations d'authentification via un service externe
     auth_service_url = "https://authentification:8001/accounts/update_profile/"
-    auth_data = {'id': user_id, 'username': username}#, 'email': email}
+    auth_data = {'id': user_id, 'username': username}
 
     try:
         auth_response = requests.post(auth_service_url, json=auth_data, verify=False)
@@ -78,7 +76,7 @@ def update_user(request):
         logging.error(f"Error calling authentication service: {e}")
         return JsonResponse({"success": False, "message": "Error calling authentication service."})
 
-    # Mise à jour ou création du profil utilisateur dans la base de données locale
+    # maj or creation profil user in bdd
     try:
         defaults = {
         'firstName': first_name,
@@ -86,7 +84,7 @@ def update_user(request):
         }
 
     
-        # Si un avatar est fourni, ajoutez-le aux valeurs par défaut pour la mise à jour/création
+        # if avatar getting, add values 
         if avatar is not None:
             defaults['avatar'] = avatar
 
@@ -97,21 +95,19 @@ def update_user(request):
 
         if profile.avatar42 is None:
             profile.avatar42 = avatar42
-            profile.save()  # Sauvegarder les modifications sur le profil
+            profile.save()
     except Profile.DoesNotExist:
-        # Si aucun profil n'existe pour cet utilisateur et qu'aucun avatar n'est fourni
+        # if nothing profile existing and nothing avatar
         profile = Profile.objects.create(
             user_id=user_id,
             firstName=first_name,
             lastName=last_name,
             avatar42=avatar42,
-            # Initialisez d'autres champs si nécessaire
         )
     except Exception as e:
         logging.error(f"Error updating or creating profile: {e}")
         return JsonResponse({"success": False, "message": "Error updating or creating profile."})
 
-    # Construction de l'URL de l'avatar
     base_url = BASE_URL + ':8001'
 
     avatar_url = base_url + profile.avatar.url if profile and profile.avatar else None
@@ -121,7 +117,6 @@ def update_user(request):
             profile.avatar = None
             profile.save()
             
-    # Réponse de succès avec les informations mises à jour
     return JsonResponse({
         "success": True,
         "message": "Profile updated successfully.",
@@ -129,27 +124,21 @@ def update_user(request):
         "firstname": profile.firstName,
         "lastname": profile.lastName,
         "username": username,
-        # "email": email,
         "avatar": avatar_url,
     })
 
 
-# @login_required
 @csrf_exempt
 @verif_sessionID
-@require_http_methods(["GET"])  # Utilisez GET si vous récupérez simplement des informations, ajustez selon besoin
-def get_user_profile(request, user_id):  # Assurez-vous que user_id est correctement capturé depuis l'URL
+@require_http_methods(["GET"])
+def get_user_profile(request, user_id):
     
-    logging.critical("YOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO")
-    logging.critical(user_id)
-
     try:
-        # Recherche du profil par user_id
         profile = Profile.objects.get(user_id=user_id)
     except Profile.DoesNotExist:
         profile = None
 
-    # Faites un appel au service d'authentification pour obtenir username et email
+    # call service authentification to get username and email
     auth_service_url = "https://authentification:8001/accounts/get_profile/"
     try:
         auth_response = requests.get(f"{auth_service_url}{user_id}", verify=False)
@@ -163,34 +152,28 @@ def get_user_profile(request, user_id):  # Assurez-vous que user_id est correcte
         logging.error(f"Error calling authentication service: {e}")
         return JsonResponse({"success": False, "message": "Error calling authentication service."})
 
-    # Construction de l'URL de l'avatar si disponible
     base_url = BASE_URL + ':8001'
 
     avatar_url = base_url + profile.avatar.url if profile and profile.avatar else None
     if profile is not None and avatar_url is None:
-        logging.critical("avatar null")
         avatar_url = profile.avatar42
-    # Réponse avec les informations récupérées
     return JsonResponse({
         "success": True,
         "firstname": profile.firstName if profile else '',
         "lastname": profile.lastName if profile else '',
-        "username": username,  # Ces informations proviennent du service d'authentification
-        "email": email,  # Ces informations proviennent du service d'authentification
+        "username": username,
+        "email": email,
         "avatar": avatar_url,
         "id": user_id,
         "avatar42": profile.avatar42 if profile else None,
     })
 
 @csrf_exempt
-# @verif_sessionID
 @require_http_methods(["DELETE"])
 def delete_user_profile(request, user_id):
-    logging.info(f"Attempting to delete profile for user_id: {user_id}")
 
     try:
         profile = Profile.objects.get(user_id=user_id)
-        logging.info(f"Profile found: {profile}")
         profile.delete()
         return JsonResponse({"success": True, "message": "Profile deleted successfully."})
     except Profile.DoesNotExist:
